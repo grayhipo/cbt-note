960047762b5ba23eddf833f11395f2d74a9df1e3
fukujou <fukujou@gmail.com>
2026-02-20 10:34:40 +0900

    remove CNAME for testing

--- vite.config.js
+++ vite.config.js
@@ -3,7 +3,7 @@ import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   plugins: [react()],
-  base: '/app/',
+  base: '/cbt-note/app/',
   build: {
     outDir: 'dist/app',
     emptyOutDir: true,
